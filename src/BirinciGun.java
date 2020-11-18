d387fe0bdea182e138863fdc7264cc6cf45e9c0c
Murat <murat.ozben1223@gmail.com>
2020-11-18 21:06:44 +0100

    master

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -15,6 +15,7 @@ public class BirinciGun {
 		System.out.println("yepyeni bir satir");
 		System.out.println("10:26 - feature branch");
 		System.out.println("10:52 - feature iki");
+		System.out.println("11:03 - master bir");
 
 
 	}
